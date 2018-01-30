1ba4ba6beec392ae1e6c0ed3a05899070667a6a7
PatrickZwernemann <35971234+PatrickZwernemann@users.noreply.github.com>
2018-01-30 19:24:45 +0100

    Create primzahl.cpp

---
+++ loop/do-whlie/primzahl.cpp
@@ -0,0 +1,32 @@
+#include <iostream.h>
+int main()   
+{ 
+	int userInput;
+	
+	do
+	{
+		cout << "Bitte geben sie eine Zahl ein: " << endl;
+    	cin >> userInput ;
+    	
+    	for (int i = 2; i <= userInput/2; i++)
+    	{
+    		if (userInput % i == 0)
+    		{
+    			cout << userInput << " ist keine Primzahl" << endl;
+    			break;
+    		}
+    		if (i == userInput/2)
+    		{
+    			cout << userInput << " ist eine Primzahl" << endl;
+    		}
+    	}
+    	
+    	
+		
+	}
+	while (userInput != 0);
+	
+	
+  getchar();
+  return 0; 
+}
